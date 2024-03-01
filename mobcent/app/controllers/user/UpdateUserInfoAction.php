595e43e8ddefe7a1eb7834febbbec3b7dd080514
jiangyanxi <jiangyanxi@live.com>
2024-03-01 15:46:39 +0800

    冻结账号

--- mobcent/app/controllers/user/UpdateUserInfoAction.php
+++ mobcent/app/controllers/user/UpdateUserInfoAction.php
@@ -16,7 +16,10 @@ class UpdateUserInfoAction extends MobcentAction {
 
     public function run($type, $gender = '', $oldPassword = '', $newPassword = '', $userInfo = '', $sign = '') {
         $res = $this->initWebApiArray();
-        if ($type == 'info') {
+        if ($type == 'freeze') {
+            global $_G;
+            C::t('common_member')->update($_G['uid'], array('freeze'=>'1'));
+        } elseif ($type == 'info') {
             $res = $this->_updateUser($res, $gender, $sign);
         } elseif ($type == 'password') {
             $res = $this->_updatePass($res, $oldPassword, $newPassword);
